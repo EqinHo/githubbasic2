3a42005fdab19e68e81da8f8ccf101de1f5b5b7a
EqinHo <Quyen.Ho@gokstadakademiet.no>
2023-01-27 12:42:17 +0100

    første commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+
